ed9c380ddb2d26f0f6d3d3337f46db7d9058872d
Simon Pelletier <seemonz@gmail.com>
2021-09-23 14:04:29 -0700

    added dontPontificate writing

--- index.py
+++ index.py
@@ -11,7 +11,18 @@ def writings():
 	return render_template('writings.html')
 
 
+
+
+# ====== WRITINGS =======
 # TODO should make this into a set of writing objects in the future
 @app.route("/writings/aStringandaBalloon")
 def aStringandaBalloon():
 	return render_template('writings/aStringandaBalloon.html')
+
+@app.route("/writings/twentyThousandLeagues")
+def twentyThousandLeagues():
+	return render_template('writings/twentyThousandLeagues.html')
+
+@app.route("/writings/dontPontificate")
+def dontPontificate():
+	return render_template('writings/dontPontificate.html')
